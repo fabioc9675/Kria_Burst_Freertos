7d18efc76196efa41916a0a101a787ef1dd42d8d
fabioc9675 <fabian.castano@udea.edu.co>
2026-01-26 23:25:19 -0500

    Creation of Vitis project and platform

---
+++ Vitis/Kria_platform/platform.tcl
@@ -0,0 +1,20 @@
+# 
+# Usage: To re-create this platform project launch xsct with below options.
+# xsct C:\GitHub\KRIA_Burst_Freertos\Vitis\Kria_platform\platform.tcl
+# 
+# OR launch xsct and run below command.
+# source C:\GitHub\KRIA_Burst_Freertos\Vitis\Kria_platform\platform.tcl
+# 
+# To create the platform in a different location, modify the -out option of "platform create" command.
+# -out option specifies the output directory of the platform project.
+
+platform create -name {Kria_platform}\
+-hw {C:\GitHub\KRIA_Burst_Freertos\Vivado\Products\kria_bd_wrapper.xsa}\
+-proc {psu_cortexa53_0} -os {freertos10_xilinx} -arch {64-bit} -fsbl-target {psu_cortexa53_0} -out {C:/GitHub/KRIA_Burst_Freertos/Vitis}
+
+platform write
+platform generate -domains 
+platform active {Kria_platform}
+platform generate
+platform config -updatehw {C:/GitHub/KRIA_Burst_Freertos/Vivado/Products/kria_bd_wrapper.xsa}
+platform generate -domains 
